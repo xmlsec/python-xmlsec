f88eeea6c047a95fcf550447bd09472bbccb7c3f
oleg.hoefling <oleg.hoefling@gmail.com>
2020-05-29 21:30:36 +0200

    add tests for wrong args in encryption, template and tree modules, cover missing functions
    Signed-off-by: oleg.hoefling <oleg.hoefling@gmail.com>

--- tests/test_templates.py
+++ tests/test_templates.py
@@ -1,7 +1,7 @@
-from tests import base
+from lxml import etree
 
 import xmlsec
-
+from tests import base
 
 consts = xmlsec.constants
 
@@ -10,20 +10,19 @@ class TestTemplates(base.TestMemoryLeaks):
     def test_create(self):
         root = self.load_xml("doc.xml")
         sign = xmlsec.template.create(
-            root,
-            c14n_method=consts.TransformExclC14N,
-            sign_method=consts.TransformRsaSha1,
-            id="Id",
-            ns="test"
+            root, c14n_method=consts.TransformExclC14N, sign_method=consts.TransformRsaSha1, id="Id", ns="test"
         )
         self.assertEqual("Id", sign.get("Id"))
         self.assertEqual("test", sign.prefix)
 
+    def test_create_bad_args(self):
+        with self.assertRaises(TypeError):
+            xmlsec.template.create('', c14n_method=consts.TransformExclC14N, sign_method=consts.TransformRsaSha1)
+
     def test_encrypt_data_create(self):
         root = self.load_xml("doc.xml")
         enc = xmlsec.template.encrypted_data_create(
-            root, method=consts.TransformDes3Cbc, id="Id", type="Type", mime_type="MimeType", encoding="Encoding",
-            ns="test"
+            root, method=consts.TransformDes3Cbc, id="Id", type="Type", mime_type="MimeType", encoding="Encoding", ns="test"
         )
         for a in ("Id", "Type", "MimeType", "Encoding"):
             self.assertEqual(a, enc.get(a))
@@ -35,18 +34,21 @@ class TestTemplates(base.TestMemoryLeaks):
         ki = xmlsec.template.ensure_key_info(sign, id="Id")
         self.assertEqual("Id", ki.get("Id"))
 
+    def test_ensure_key_info_fail(self):
+        with self.assertRaisesRegex(xmlsec.InternalError, 'cannot ensure key info.'):
+            xmlsec.template.ensure_key_info(etree.fromstring(b'<Data/>'), id="Id")
+
+    def test_ensure_key_info_bad_args(self):
+        with self.assertRaises(TypeError):
+            xmlsec.template.ensure_key_info('', id=0)
+
     def test_add_encrypted_key(self):
         root = self.load_xml("doc.xml")
         sign = xmlsec.template.create(root, c14n_method=consts.TransformExclC14N, sign_method=consts.TransformRsaSha1)
         ki = xmlsec.template.ensure_key_info(sign)
         ek = xmlsec.template.add_encrypted_key(ki, consts.TransformRsaOaep)
-        self.assertEqual(
-            ek,
-            xmlsec.tree.find_node(self.load_xml("sign_template.xml"), consts.NodeEncryptedKey, consts.EncNs)
-        )
-        ek2 = xmlsec.template.add_encrypted_key(
-            ki, consts.TransformRsaOaep, id="Id", type="Type", recipient="Recipient"
-        )
+        self.assertEqual(ek, xmlsec.tree.find_node(self.load_xml("sign_template.xml"), consts.NodeEncryptedKey, consts.EncNs))
+        ek2 = xmlsec.template.add_encrypted_key(ki, consts.TransformRsaOaep, id="Id", type="Type", recipient="Recipient")
         for a in ("Id", "Type", "Recipient"):
             self.assertEqual(a, ek2.get(a))
 
@@ -55,12 +57,21 @@ class TestTemplates(base.TestMemoryLeaks):
         sign = xmlsec.template.create(root, c14n_method=consts.TransformExclC14N, sign_method=consts.TransformRsaSha1)
         ki = xmlsec.template.ensure_key_info(sign)
         kn = xmlsec.template.add_key_name(ki)
-        self.assertEqual(
-            kn,
-            xmlsec.tree.find_node(self.load_xml("sign_template.xml"), consts.NodeKeyName, consts.DSigNs)
-        )
+        self.assertEqual(kn, xmlsec.tree.find_node(self.load_xml("sign_template.xml"), consts.NodeKeyName, consts.DSigNs))
         kn2 = xmlsec.template.add_key_name(ki, name="name")
         self.assertEqual("name", kn2.text)
+
+    def test_add_key_name_none(self):
+        root = self.load_xml("doc.xml")
+        sign = xmlsec.template.create(root, c14n_method=consts.TransformExclC14N, sign_method=consts.TransformRsaSha1)
+        ki = xmlsec.template.ensure_key_info(sign)
+        kn2 = xmlsec.template.add_key_name(ki, name=None)
+        self.assertEqual(kn2.text, None)
+        print(etree.tostring(kn2))
+
+    def test_add_key_name_bad_args(self):
+        with self.assertRaises(TypeError):
+            xmlsec.template.add_key_name('')
 
     def test_add_reference(self):
         root = self.load_xml("doc.xml")
@@ -69,15 +80,32 @@ class TestTemplates(base.TestMemoryLeaks):
         for a in ("Id", "URI", "Type"):
             self.assertEqual(a, ref.get(a))
 
+    def test_add_reference_bad_args(self):
+        with self.assertRaises(TypeError):
+            xmlsec.template.add_reference('', consts.TransformSha1)
+        with self.assertRaises(TypeError):
+            xmlsec.template.add_reference(etree.Element('root'), '')
+
+    def test_add_reference_fail(self):
+        with self.assertRaisesRegex(xmlsec.InternalError, 'cannot add reference.'):
+            xmlsec.template.add_reference(etree.Element('root'), consts.TransformSha1)
+
+    def test_add_transform_bad_args(self):
+        with self.assertRaises(TypeError):
+            xmlsec.template.add_transform('', consts.TransformSha1)
+        with self.assertRaises(TypeError):
+            xmlsec.template.add_transform(etree.Element('root'), '')
+
     def test_add_key_value(self):
         root = self.load_xml("doc.xml")
         sign = xmlsec.template.create(root, c14n_method=consts.TransformExclC14N, sign_method=consts.TransformRsaSha1)
         ki = xmlsec.template.ensure_key_info(sign)
         kv = xmlsec.template.add_key_value(ki)
-        self.assertEqual(
-            kv,
-            xmlsec.tree.find_node(self.load_xml("sign_template.xml"), consts.NodeKeyValue, consts.DSigNs)
-        )
+        self.assertEqual(kv, xmlsec.tree.find_node(self.load_xml("sign_template.xml"), consts.NodeKeyValue, consts.DSigNs))
+
+    def test_add_key_value_bad_args(self):
+        with self.assertRaises(TypeError):
+            xmlsec.template.add_key_value('')
 
     def test_add_x509_data(self):
         root = self.load_xml("doc.xml")
@@ -91,10 +119,11 @@ class TestTemplates(base.TestMemoryLeaks):
         xmlsec.template.x509_data_add_subject_name(x509)
         xmlsec.template.x509_issuer_serial_add_issuer_name(issuer)
         xmlsec.template.x509_issuer_serial_add_serial_number(issuer)
-        self.assertEqual(
-            x509,
-            xmlsec.tree.find_node(self.load_xml("sign_template.xml"), consts.NodeX509Data, consts.DSigNs)
-        )
+        self.assertEqual(x509, xmlsec.tree.find_node(self.load_xml("sign_template.xml"), consts.NodeX509Data, consts.DSigNs))
+
+    def test_add_x509_data_bad_args(self):
+        with self.assertRaises(TypeError):
+            xmlsec.template.add_x509_data('')
 
     def test_x509_issuer_serial_add_issuer(self):
         root = self.load_xml("doc.xml")
@@ -107,26 +136,64 @@ class TestTemplates(base.TestMemoryLeaks):
         self.assertEqual("Name", name.text)
         self.assertEqual("Serial", serial.text)
 
+    def test_x509_issuer_serial_add_issuer_bad_args(self):
+        with self.assertRaises(TypeError):
+            xmlsec.template.x509_data_add_issuer_serial('')
+
+    def test_x509_issuer_serial_add_issuer_name_bad_args(self):
+        with self.assertRaises(TypeError):
+            xmlsec.template.x509_issuer_serial_add_issuer_name('')
+
+    def test_x509_issuer_serial_add_serial_number_bad_args(self):
+        with self.assertRaises(TypeError):
+            xmlsec.template.x509_issuer_serial_add_serial_number('')
+
+    def test_x509_data_add_subject_name_bad_args(self):
+        with self.assertRaises(TypeError):
+            xmlsec.template.x509_data_add_subject_name('')
+
+    def test_x509_data_add_ski_bad_args(self):
+        with self.assertRaises(TypeError):
+            xmlsec.template.x509_data_add_ski('')
+
+    def test_x509_data_add_certificate_bad_args(self):
+        with self.assertRaises(TypeError):
+            xmlsec.template.x509_data_add_certificate('')
+
+    def test_x509_data_add_crl_bad_args(self):
+        with self.assertRaises(TypeError):
+            xmlsec.template.x509_data_add_crl('')
+
+    def test_add_encrypted_key_bad_args(self):
+        with self.assertRaises(TypeError):
+            xmlsec.template.add_encrypted_key('', 0)
+
+    def test_encrypted_data_create_bad_args(self):
+        with self.assertRaises(TypeError):
+            xmlsec.template.encrypted_data_create('', 0)
+
     def test_encrypted_data_ensure_cipher_value(self):
         root = self.load_xml("doc.xml")
         enc = xmlsec.template.encrypted_data_create(root, method=consts.TransformDes3Cbc)
         cv = xmlsec.template.encrypted_data_ensure_cipher_value(enc)
-        self.assertEqual(
-            cv,
-            xmlsec.tree.find_node(self.load_xml("sign_template.xml"), consts.NodeCipherValue, consts.EncNs)
-        )
+        self.assertEqual(cv, xmlsec.tree.find_node(self.load_xml("sign_template.xml"), consts.NodeCipherValue, consts.EncNs))
+
+    def test_encrypted_data_ensure_cipher_value_bad_args(self):
+        with self.assertRaises(TypeError):
+            xmlsec.template.encrypted_data_ensure_cipher_value('')
 
     def test_encrypted_data_ensure_key_info(self):
         root = self.load_xml("doc.xml")
         enc = xmlsec.template.encrypted_data_create(root, method=consts.TransformDes3Cbc)
         ki = xmlsec.template.encrypted_data_ensure_key_info(enc)
-        self.assertEqual(
-            ki,
-            xmlsec.tree.find_node(self.load_xml("enc_template.xml"), consts.NodeKeyInfo, consts.DSigNs)
-        )
+        self.assertEqual(ki, xmlsec.tree.find_node(self.load_xml("enc_template.xml"), consts.NodeKeyInfo, consts.DSigNs))
         ki2 = xmlsec.template.encrypted_data_ensure_key_info(enc, id="Id", ns="test")
         self.assertEqual("Id", ki2.get("Id"))
         self.assertEqual("test", ki2.prefix)
+
+    def test_encrypted_data_ensure_key_info_bad_args(self):
+        with self.assertRaises(TypeError):
+            xmlsec.template.encrypted_data_ensure_key_info('')
 
     def test_transform_add_c14n_inclusive_namespaces(self):
         root = self.load_xml("doc.xml")
@@ -136,7 +203,8 @@ class TestTemplates(base.TestMemoryLeaks):
         xmlsec.template.transform_add_c14n_inclusive_namespaces(trans1, "default")
         trans2 = xmlsec.template.add_transform(ref, consts.TransformXslt)
         xmlsec.template.transform_add_c14n_inclusive_namespaces(trans2, ["ns1", "ns2"])
-        self.assertEqual(
-            ref,
-            xmlsec.tree.find_node(self.load_xml("sign_template.xml"), consts.NodeReference, consts.DSigNs)
-        )
+        self.assertEqual(ref, xmlsec.tree.find_node(self.load_xml("sign_template.xml"), consts.NodeReference, consts.DSigNs))
+
+    def test_transform_add_c14n_inclusive_namespaces_bad_args(self):
+        with self.assertRaises(TypeError):
+            xmlsec.template.transform_add_c14n_inclusive_namespaces('', [])
--- tests/test_tree.py
+++ tests/test_tree.py
@@ -1,7 +1,5 @@
-from tests import base
-
 import xmlsec
-
+from tests import base
 
 consts = xmlsec.constants
 
@@ -14,11 +12,19 @@ class TestTree(base.TestMemoryLeaks):
         self.assertIsNone(xmlsec.tree.find_child(root, consts.NodeReference))
         self.assertIsNone(xmlsec.tree.find_child(root, consts.NodeSignedInfo, consts.EncNs))
 
+    def test_find_child_bad_args(self):
+        with self.assertRaises(TypeError):
+            xmlsec.tree.find_child('', 0, True)
+
     def test_find_parent(self):
         root = self.load_xml("sign_template.xml")
         si = xmlsec.tree.find_child(root, consts.NodeSignedInfo, consts.DSigNs)
         self.assertIs(root, xmlsec.tree.find_parent(si, consts.NodeSignature))
         self.assertIsNone(xmlsec.tree.find_parent(root, consts.NodeSignedInfo))
+
+    def test_find_parent_bad_args(self):
+        with self.assertRaises(TypeError):
+            xmlsec.tree.find_parent('', 0, True)
 
     def test_find_node(self):
         root = self.load_xml("sign_template.xml")
@@ -26,6 +32,14 @@ class TestTree(base.TestMemoryLeaks):
         self.assertEqual(consts.NodeReference, ref.tag.partition('}')[2])
         self.assertIsNone(xmlsec.tree.find_node(root, consts.NodeReference, consts.EncNs))
 
+    def test_find_node_bad_args(self):
+        with self.assertRaises(TypeError):
+            xmlsec.tree.find_node('', 0, True)
+
     def test_add_ids(self):
         root = self.load_xml("sign_template.xml")
         xmlsec.tree.add_ids(root, ["id1", "id2", "id3"])
+
+    def test_add_ids_bad_args(self):
+        with self.assertRaises(TypeError):
+            xmlsec.tree.add_ids('', [])
