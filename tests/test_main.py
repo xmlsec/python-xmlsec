91730b25986284304da480c62f4f1dbf6cd44de0
Paul Weaver <paul@concertdaw.co.uk>
2021-07-30 11:02:00 +0100

    Test and fix IO callback bindings
    Turns out the `xmlSecAllIOCallbacks` pointer list yields the stored
    callbacks in reverse, _and_ the default callbacks steal everything (
    libxml2's `xmlFileMatch` is literally defined as `return(1)`! So we
    
     - Simplify our linked list of sets of Python callbacks to cons, rather
       than append
     - Don't bother trying to track interleaving default callbacks and Python
       callbacks, because `cur_cb_list_item` would get left in a bad state when
       the default matched.
     - Instead, drop all callbacks added prior to the default callbacks being
       added.
    
    Tests-wise, there are a lot of failures relating to unreferenced objects
    and/or memory leaks. However, as an example the top test is completely
    commented out right now (i.e. doesn't do anything with the library), so
    I'm a bit stuck as to how to grapple with that!

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,4 +1,10 @@
 import xmlsec
+from xmlsec import constants as consts
+
+from io import BytesIO
+
+from hypothesis import given, strategies
+import pytest
 from tests import base
 
 
@@ -30,3 +36,131 @@ class TestBase64LineSize(base.TestMemoryLeaks):
         with self.assertRaises(ValueError):
             xmlsec.base64_default_line_size(-1)
         self.assertEqual(xmlsec.base64_default_line_size(), size)
+
+
+class TestCallbacks(base.TestMemoryLeaks):
+    def setUp(self):
+        super().setUp()
+        xmlsec.cleanup_callbacks()
+
+    @given(funcs=strategies.lists(
+        strategies.sampled_from([
+            lambda: None
+            # xmlsec.cleanup_callbacks,
+            # xmlsec.register_default_callbacks,
+        ])
+    ))
+    def test_arbitrary_cleaning_and_default_callback_registration(self, funcs):
+        # FIXME: This test seems to detelct unreferenced objects and memory
+        # leaks even if it never does anything!
+        pass
+        # for f in funcs:
+        #     f()
+
+    def _sign_doc(self):
+        root = self.load_xml("doc.xml")
+        sign = xmlsec.template.create(
+            root,
+            c14n_method=consts.TransformExclC14N,
+            sign_method=consts.TransformRsaSha1
+        )
+        xmlsec.template.add_reference(
+            sign, consts.TransformSha1, uri="cid:123456")
+
+        ctx = xmlsec.SignatureContext()
+        ctx.key = xmlsec.Key.from_file(
+            self.path("rsakey.pem"), format=consts.KeyDataFormatPem
+        )
+        ctx.sign(sign)
+        return sign
+
+    def _expect_sign_failure(self):
+        exc_info = pytest.raises(xmlsec.Error, self._sign_doc)
+        self.assertEqual(exc_info.value.args, (1, 'failed to sign'))
+
+    def _register_mismatch_callbacks(self, match_cb=lambda filename: False):
+        xmlsec.register_callbacks(
+            match_cb,
+            lambda filename: None,
+            lambda none, buf: 0,
+            lambda none: None,
+        )
+
+    def _register_match_callbacks(self):
+        xmlsec.register_callbacks(
+            lambda filename: filename == b'cid:123456',
+            lambda filename: BytesIO(b'<html><head/><body/></html>'),
+            lambda bio, buf: bio.readinto(buf),
+            lambda bio: bio.close(),
+        )
+
+    def _find(self, elem, *tags):
+        for tag in tags:
+            elem = elem.find(
+                '{{http://www.w3.org/2000/09/xmldsig#}}{}'.format(tag))
+        return elem
+
+    def _verify_external_data_signature(self):
+        signature = self._sign_doc()
+        digest = self._find(
+            signature, 'SignedInfo', 'Reference', 'DigestValue'
+        ).text
+        self.assertEqual(digest, 'VihZwVMGJ48NsNl7ertVHiURXk8=')
+
+    def test_sign_external_data_no_callbacks_fails(self):
+        self._expect_sign_failure()
+
+    def test_sign_external_data_default_callbacks_fails(self):
+        xmlsec.register_default_callbacks()
+        self._expect_sign_failure()
+
+    def test_sign_external_data_no_matching_callbacks_fails(self):
+        self._register_mismatch_callbacks()
+        self._expect_sign_failure()
+
+    def test_sign_data_from_callbacks(self):
+        self._register_match_callbacks()
+        self._verify_external_data_signature()
+
+    @given(
+        num_prior_mismatches=strategies.integers(min_value=1, max_value=50),
+        num_post_mismatches=strategies.integers(min_value=1, max_value=50),
+    )
+    def test_sign_data_not_first_callback(
+            self, num_prior_mismatches, num_post_mismatches
+    ):
+        bad_match_calls = 0
+
+        def match_cb(filename):
+            nonlocal bad_match_calls
+            bad_match_calls += 1
+            False
+
+        for _ in range(num_post_mismatches):
+            self._register_mismatch_callbacks(match_cb)
+
+        self._register_match_callbacks()
+
+        for _ in range(num_prior_mismatches):
+            self._register_mismatch_callbacks()
+
+        self._verify_external_data_signature()
+        self.assertEqual(bad_match_calls, 0)
+
+    def test_failed_sign_because_default_callbacks(self):
+        mismatch_calls = 0
+
+        def mismatch_cb(filename):
+            nonlocal mismatch_calls
+            mismatch_calls += 1
+            False
+
+        # NB: These first two sets of callbacks should never get called,
+        # because the default callbacks always match beforehand:
+        self._register_match_callbacks()
+        self._register_mismatch_callbacks(mismatch_cb)
+        xmlsec.register_default_callbacks()
+        self._register_mismatch_callbacks(mismatch_cb)
+        self._register_mismatch_callbacks(mismatch_cb)
+        self._expect_sign_failure()
+        self.assertEqual(mismatch_calls, 2)
