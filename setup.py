906a0e2c534ddbe35214bfeb86760b9438070b41
Clément Escolano <clement.escolano@icloud.com>
2025-10-31 14:22:54 +0100

    Add support for Python 3.14

--- setup.py
+++ setup.py
@@ -50,6 +50,7 @@ setup(
         'Programming Language :: Python :: 3.11',
         'Programming Language :: Python :: 3.12',
         'Programming Language :: Python :: 3.13',
+        'Programming Language :: Python :: 3.14',
         'Topic :: Text Processing :: Markup :: XML',
         'Typing :: Typed',
     ],
